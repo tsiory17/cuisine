3c283242f5f4fa3dcb8e395771b200d055d3c1ac
Rako <tsiory.r17@gmail.con>
2025-03-07 01:42:35 -0500

    created the header and the form component

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+export default function Header (){
+  return(
+    <div className="header">
+      <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/91631e116794391.60698dbc55f96.jpg" alt="logo" />
+      <h1>Cuisine</h1>
+
+    </div>
+  )
+}
